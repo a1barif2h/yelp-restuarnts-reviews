388e5d3c56208d1b10909f5a6728e862b5c0b59a
a1barif2h <a1barif2h@gmail.com>
2021-11-22 15:58:56 +0600

    create, update, delete restaurants api is done

--- resturant-review-app/server/controllers/restaurants.js
+++ resturant-review-app/server/controllers/restaurants.js
@@ -1,33 +1,55 @@
 const { StatusCodes } = require('http-status-codes')
 
 const DB = require('../DB');
-const { NotFoundError } = require('../error');
+const { NotFoundError, BadRequestError } = require('../error');
 
 const getAllRestaurants = async (req, res) => {
-    const data = await DB.query('SELECT * FROM restaurants')
+    const data = await DB.query('SELECT * FROM restaurants ORDER BY name ASC')
+    if(!data.rowCount) {
+        throw new NotFoundError(`No restaurants added yet.`)
+    }
     res.status(StatusCodes.OK).json({msg: 'Success', results: data.rowCount, restaurants: data.rows})
 };
 
 const getRestaurant = async (req, res) => {
     const {params: {id}} = req
-    const data = await DB.query(`SELECT * FROM restaurants WHERE id=${id}`)
-    if(data.rowCount) {
-        res.status(StatusCodes.OK).json({msg: 'Success', results: data.rowCount, restaurant: data.rows[0]})
-    } else {
+    const data = await DB.query(`SELECT * FROM restaurants WHERE id=$1`, [id])
+    if(!data.rowCount) {
         throw new NotFoundError(`No data found with id ${id}`)
     }
+    res.status(StatusCodes.OK).json({msg: 'Success', results: data.rowCount, restaurant: data.rows[0]})
 };
 
 const createRestaurant = async (req, res) => {
-    res.send('create a restaurant')
+    const {body} = req
+    if(!body.name || !body.location || !body.price_range) {
+        throw new BadRequestError('Name, Location, Price Range are required, Please check all fields again.')
+    }
+    await DB.query(`INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3)`, [body.name, body.location, body.price_range])
+    res.status(StatusCodes.CREATED).json({msg: 'Success', restaurant: body})
 };
 
 const updateRestaurant = async (req, res) => {
-    res.send('update a restaurant')
+    const {body, params: {id}} = req
+    if(!body.name || !body.location || !body.price_range) {
+        throw new BadRequestError('Name, Location, Price Range are required, Please check all fields again.')
+    }
+    const data = await DB.query(`UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *`, [body.name, body.location, body.price_range, id])
+    if(!data.rowCount) {
+        throw new NotFoundError(`No data found with id ${id}`)
+
+    }
+    res.status(StatusCodes.OK).json({msg: 'Success', restaurant: data.rows[0]})
+    
 };
 
 const deleteRestaurant = async (req, res) => {
-    res.send('delete restaurant')
+    const {params: {id}} = req
+    const data = await DB.query(`DELETE FROM restaurants WHERE id = $1`, [id])
+    if(!data.rowCount) {
+        throw new NotFoundError(`No data found with id ${id}`)
+    }
+    res.status(StatusCodes.OK).json({msg: 'Success'})
 };
 
 module.exports = {
